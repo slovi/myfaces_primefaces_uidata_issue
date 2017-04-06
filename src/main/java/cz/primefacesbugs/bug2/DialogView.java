bd6e3c9d4f16a9b51c45e8ff0c038a7875b0ceee
_ext Slovak, Jiri <jiri.slovak.ext@dieboldnixdorf.com>
2017-04-07 00:56:31 +0200

    refactor - dialog names

--- src/main/java/cz/primefacesbugs/bug2/DialogView.java
+++ src/main/java/cz/primefacesbugs/bug2/DialogView.java
@@ -1,9 +1,6 @@
 package cz.primefacesbugs.bug2;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -18,15 +15,11 @@ public class DialogView implements Serializable {
 	private static final long serialVersionUID = 7123165280133112773L;
 
 	public void showDialog1(ActionEvent e) {
-		Map<String, Object> options = new HashMap<String, Object>();
-		options.put("modal", true);
-		options.put("closable", true);
-		RequestContext.getCurrentInstance().openDialog(
-				"/bug2/test-dialog-1", options, new HashMap<String, List<String>>());
+		RequestContext.getCurrentInstance().openDialog("/bug2/dialog1");
 	}
 	
 	public void showDialog2(ActionEvent e) {
-		RequestContext.getCurrentInstance().openDialog("/bug2/test-dialog-2");
+		RequestContext.getCurrentInstance().openDialog("/bug2/dialog2");
 	}
 	
 	public void closeCurrentDialog(ActionEvent e) {
