adff0c370919e67ee01117d8a5f63f1e0e3cf370
_ext Slovak, Jiri <jiri.slovak.ext@dieboldnixdorf.com>
2017-10-26 13:55:41 +0200

    File upload test

---
+++ primefaces-bugs-web/src/main/java/cz/primefacesbugs/test1/FileUploadView.java
@@ -0,0 +1,39 @@
+package cz.primefacesbugs.test1;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+@Named
+@ViewScoped
+public class FileUploadView implements Serializable {
+	
+	private UploadedFile file;
+	 
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+     
+    public void upload() {
+        if (file != null) {
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+    
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+}
